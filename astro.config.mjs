5373ab987a354bf13f5403e78ac5c4d2eb837c32
Bulat <bulat067@gmail.com>
2025-06-25 09:59:24 +0300

    Trigger deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// // @ts-check
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  output: "static",
+  base: "/agroton/",
+  site: "https://breadblead.github.io/agroton",
+});
